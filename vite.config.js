0e2ccd625a59005fb5103b0047ae385caf0254d4
Harshit <hpurigoswami786@gmail.com>
2024-03-09 00:29:41 +0530

    vite.config.js in add some changes for create a multi build and ./ add this  in src and href

--- vite.config.js
+++ vite.config.js
@@ -22,23 +22,50 @@
 //   }
 // })
 
+// import { resolve } from 'path'
+// import { defineConfig } from 'vite'
+
+// const root = resolve(__dirname, 'src');
+// const outDir = resolve(__dirname, 'dist'); 
+
+// export default defineConfig({
+//   // base: './',
+//   root,
+//   emptyOutDir: ture,
+//   build: {
+//     outDir,
+//     rollupOptions: { 
+//       main: resolve(__dirname, 'index.html'),
+//       about: resolve(__dirname, 'about.html'),
+//       contact: resolve(__dirname, 'contact.html'),
+//     },
+//       server: {
+//     port: 8080
+//   }
+//   }
+// })
+
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 const root = resolve(__dirname, 'src');
-const outDir = resolve(__dirname, 'dist'); 
+const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
   root,
+  base: './',
   build: {
     outDir,
     rollupOptions: {
-      main : 'index.html',
-      about : 'about.html' 
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'about.html'),
+        contact: resolve(root, 'contact.html'),
+      },
     },
-      server: {
+  },
+  server: {
     port: 8080
   }
-  },
 })
 
